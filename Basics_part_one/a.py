61d56261855ec04726b692f63ff2a25655ac1601
Aaron Gebremaraim <mulabr673@gmail.com>
2023-10-23 11:26:02 +0300

    basic part one

---
+++ Basics_part_one/a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+# Fundamental Datatypes
+int
+bool
+float
+str
+list
+tuple
+set
+dict
+# custom datatypes
+class
+
+# Specialized datatypes
+Modules
+# None
+
+'''
+print(type('AAron'))
+print(type(2/4))
+print(type(2//4))
