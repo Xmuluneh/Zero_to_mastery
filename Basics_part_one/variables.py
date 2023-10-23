819f4cbd4f5781c0057d3ab01c89c3e73d59d9e2
Aaron Gebremaraim <mulabr673@gmail.com>
2023-10-24 00:03:25 +0300

    variables

---
+++ Basics_part_one/variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+
+
+'''
+import math as m
+
+my_variable_name = 'Hello from the following page'
+a, b, c, f = 8, 2, 4, 5
+print(my_variable_name)
+print(f)
+print(type(my_variable_name))
+print(m.pi)
